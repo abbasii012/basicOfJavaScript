e78ca6dc8d92f2e3057b1c03a818c7995a59d6b1
abbasi012 <haseebsajjad666@gmail.com>
2024-09-01 22:58:22 +0500

    comparison in JS done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2!=1);
+// console.log(2==1);
+
+
+
+// console.log("2">1);
+// console.log("02"<1);  // always make sure that the both sides data types should
+                        //    be same and also for avoid confusion..
+
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null!=0);
+// console.log(null<=0);
+// console.log(null>=0);
+
+
+
+console.log("2"===2);
+
+
