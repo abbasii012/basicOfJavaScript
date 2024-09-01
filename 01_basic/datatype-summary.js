62b95f020834e61f617eaf24c2b4b21e80f83edd
abbasi012 <haseebsajjad666@gmail.com>
2024-09-02 00:13:24 +0500

    stack and heap in data types summary

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -12,13 +12,13 @@ let id =Symbol("123")
 let anotherid =Symbol("123")
 // console.log(id==anotherid);
 let numbber =124524523452354252n
-console.log(typeof score)
-console.log(typeof valuescore)
-console.log(typeof outsidetemp)
-console.log(typeof username)
-console.log(typeof isloggedin)
-console.log(typeof id)
-console.log(typeof numbber)
+// console.log(typeof score)
+// console.log(typeof valuescore)
+// console.log(typeof outsidetemp)
+// console.log(typeof username)
+// console.log(typeof isloggedin)
+// console.log(typeof id)
+// console.log(typeof numbber)
 
 
 //NON-primitive(reference)
@@ -35,7 +35,29 @@ const myfun = function(){
 console.log("hello worlds");
 
 }
-console.log(typeof scores)
-console.log(typeof myValue)
-console.log(typeof myfun);
+// console.log(typeof scores)
+// console.log(typeof myValue)
+// console.log(typeof myfun);
 
+
+//============Stack and Heap===========
+//stack (primitive), heap (non -primitive)
+
+
+let mychannel = "haseeb@"
+let anotherchannel = mychannel
+anotherchannel= "abbasi@"
+
+console.log(mychannel)
+console.log(anotherchannel)
+
+let user1={
+    email:"haseb@google.com",
+    upi:"upi12"
+}
+let user2=user1
+user2.email="abbasi@gmail.com"
+
+console.log(user1)
+console.log(user2)
+// stack will take a copy and heap will take the reference of objects
