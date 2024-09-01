39e1009a8d59c15ec82d00ad608d0dac4ee4da88
abbasi012 <haseebsajjad666@gmail.com>
2024-09-01 20:27:46 +0500

    data types in javaScript

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const accountId =  437373
+let accountemail= "haseebsajjad@gmail.com"
+var accountPassword = "12345"
+accountCity = "rawalakot"
+let accountState
+
+
+// accountId=2   not allowed
+// console.log(accountId)
+accountemail= 'haseeeb@'
+accountPassword = '12345678'
+accountCity  = 'bagh'
+
+console.table([accountemail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,19 @@
+// "use strict";  // treat all JS code code as newer version;
+
+// alert(hello)       //we are using node js, not browser.
+
+let name = "haseeb"
+let age = 20
+let isloggedIn = false
+
+//number =>   2 to power 53 
+// bigint
+// string => ""
+//bool => true or false
+//null => standalone value {showing empty values} e.g let age = null
+//undefined e.g let state;
+//symbol => unique (mostly use in react)
+//object  
+
+console.log(typeof undefined)
+console.log(typeof null)
