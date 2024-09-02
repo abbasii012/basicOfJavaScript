3bc41624d009e18283b065ebd9c1cc51484a31d8
abbasi012 <haseebsajjad666@gmail.com>
2024-09-03 00:49:18 +0500

    basic of function

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -5,12 +5,42 @@
 // console.log(add(4,9))
 
 
-function userloggedin(username){
-    if(!username){
-        console.log("please ender the name");
-        return
+// function userloggedin(username){
+//     if(!username){
+//         console.log("please ender the name");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// console.log(userloggedin("haseeb"))
+
+// function calculatecard(var1,var2,...num1) {
+//     return num1
+// }
+
+
+// console.log(calculatecard(7,8,9,10))
+
+const user = {
+        name : "haseeb",
+        age: 18,
     }
-    return `${username} just logged in`
+// function calculatecard(object) {
+//     return `my name is ${object.name} and age is ${object.age}`
+// }
+
+
+// console.log(calculatecard(user))
+
+
+
+
+
+const myarray =[10,10,10]
+function calculatecard(array) {
+        return array
+
 }
 
-console.log(userloggedin("haseeb"))
+console.log(calculatecard(myarray))
