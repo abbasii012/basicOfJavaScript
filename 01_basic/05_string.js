f8fe1784505f9579992b1a5be0f592ce10d8760e
abbasi012 <haseebsajjad666@gmail.com>
2024-09-02 15:43:40 +0500

    explore string, number and math in JS

---
+++ 01_basic/05_string.js
@@ -0,0 +1,33 @@
+const name = "haseeb"
+const repoCount = 50
+
+// console.log(name + repoCount + " value")
+
+// string interpolation
+
+// console.log(`my name is ${name} and my total repo in GitHUb in ${repoCount}`)
+
+// another way to delcered a string is 
+
+const gameCount = new String("haseeb")
+
+// console.log(gameCount)
+// console.log(name[0])
+// console.log(gameCount[0])
+// console.log(gameCount.length)
+// console.log(gameCount.toUpperCase())
+// console.log(gameCount.toLowerCase())
+// console.log(gameCount.charAt(5))
+// console.log(gameCount.indexOf("h"))
+// console.log(gameCount.substring(0,4))
+// console.log(gameCount.slice(-1))
+
+// console.log(gameCount.trim())  // remove space from start and end
+// console.log(gameCount.replace("has","abbasi"))  
+
+//  console.log(gameCount.includes("has"))
+// const newString = new String("haseeb-sajjad-abbasi")
+// console.log(newString.split("-"))
+
+
+
---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,38 @@
+const score = 100
+// console.log(score)
+
+
+const balance = new Number(200)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const newNum = 1231.34963
+
+// console.log(newNum.toPrecision(4));
+
+
+
+const newNum2 = 10000000
+// console.log(newNum2.toLocaleString());
+
+
+// +++++++++++++++++++++ Math +++++++++++++++++++++
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.8))
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(4.8))
+// console.log(Math.min(4,5,6,7))
+// console.log(Math.max(4,5,6,7))
+
+// console.log(Math.random())
+
+// console.log(Math.floor((Math.random()*10)+ 1))
+
+const min = 1 
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
