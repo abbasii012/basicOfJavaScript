20258679c233cca20f74632730fdc99c24294f41
abbasi012 <haseebsajjad666@gmail.com>
2024-09-04 00:11:33 +0500

    iteration in JS for loo

---
+++ 05_iteration/01_for_loop.js
@@ -0,0 +1,33 @@
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+    
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log("tables of ",i);
+    
+//    for (let j = 1; j <= 10; j++) {
+//     console.log(i + "*" + j + "=" + i*j); 
+//    }  
+// }
+
+//break and continues
+
+// for (let i = 0; i <=10; i++) {
+//     if(i==5){
+//         console.log("5 is best");
+//         break  
+//     }
+//     console.log(i);
+// }
+
+
+
+for (let i = 0; i <=10; i++) {
+    if(i==5){
+        console.log("5 is best");
+        continue 
+    }
+    console.log(i);
+}
