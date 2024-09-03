0d7cd603dd516b46e51f2d455acf01430195f902
abbasi012 <haseebsajjad666@gmail.com>
2024-09-03 16:13:23 +0500

    arrow and IIFE compelete

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,81 @@
+//  const user ={
+//     username  : "haseeb",
+//     rollno : "012",
+//     welcomeMessage : function(){
+//         console.log(`${this.username} , welcome to our objects`)
+//         console.log(this)
+//     }
+//  }
+
+//  console.log(user.username);
+//  user.welcomeMessage()
+//  user.username = "sam"
+//  console.log(user.username);
+// user.welcomeMessage()
+ 
+
+
+// const welcomeMessage = function(){
+//             let username ="haseeb"
+//             console.log(this)
+//         }
+
+// welcomeMessage()
+
+// function welcomeMessage1(){
+//      const user ={
+//     username  : "haseeb",
+//     rollno : "012",
+//     welcomeMessage : function(){
+//         console.log(`${this.username} , welcome to our objects`)
+//         console.log(this)
+//     }
+//  }
+//     // let username ="haseeb"
+//     // console.log(this)
+//    user.welcomeMessage()
+    
+// }
+
+// welcomeMessage1()
+
+
+// const welcomeMessage1 = () => {
+//            const user ={
+//     username  : "haseeb",
+//     rollno : "012",
+//     welcomeMessage : function(){
+//         console.log(`${this.username} , welcome to our objects`)
+//         console.log(this)
+//     }
+//  }
+    // let username ="haseeb"
+    // console.log(this)
+//    user.welcomeMessage()
+            // }
+
+
+            // welcomeMessage1()
+
+
+
+// const welcomeMessage = () => {
+//           let username ="haseeb"
+//                 console.log(this)
+//             }
+
+
+//             welcomeMessage()
+
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2
+
+// }
+
+// const addTwo = (num1,num2)=> num1 + num2 
+
+// const addTwo = (num1,num2)=> (num1 + num2)
+
+// const addTwo = (num1,num2)=> ({user:"haseeb"})
+
+// console.log(addTwo(4,5))
---
+++ 03_basic/04_iife.js
@@ -0,0 +1,19 @@
+// IIFE
+// (function chai(){
+//     //named iife 
+//     console.log("DB connection done 1");
+    
+// })();
+
+// ((name)=>{
+//     console.log("DB connection done 2",name);
+// })("haseeb")
+
+// const result = (function chai(num1,num2){
+//     //named iife 
+//     return num1 + num2
+    
+// })(4,5);
+
+
+
