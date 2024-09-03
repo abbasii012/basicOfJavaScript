bfa4c79aff0404533d09cbb180fe4f231980ae86
abbasi012 <haseebsajjad666@gmail.com>
2024-09-03 13:49:38 +0500

    scope and mini hosting

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,36 @@
+// let a = 1
+// const b = 8
+// var c = 10
+
+// if(true){
+//     let a = 11
+// const b = 81
+// var c = 101
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// function one(){
+//     const username = "haseeb"
+//     function two() {
+//         const wepsite = " youtube"
+//         console.log(username + wepsite)
+//     }
+//     two()
+//     // console.log(wepsite)
+// }
+// one()
+// console.log(username)
+
+
+// console.log(add(2))
+// function add(num){
+//     return num+2
+// }
+
+
+// console.log(add(2))
+// const add = function(num){
+//     return num+2
+// }
